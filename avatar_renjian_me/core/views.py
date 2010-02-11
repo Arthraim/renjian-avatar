85ff74f5b5786c63ff48f83af5119808173e35f8
Arthur Wang <arthraim@gmail.com>
2010-02-11 14:29:48 +0800

    Seperate history pages.

--- avatar_renjian_me/core/views.py
+++ avatar_renjian_me/core/views.py
@@ -66,9 +66,42 @@ def error(request):
     return render_to_response('error.html', {'message': message})
 
 def history(request):
-    myQ = Renjianer.objects.order_by('-date')
-    cpQ = Renjianer.objects.all()
-    cpQ.query.group_by = ['user_name']
-    usernum = cpQ.__len__()
-    visitnum = myQ.__len__()
-    return render_to_response('history.html', {'num': usernum, 'visitnum': visitnum, 'rens': myQ})
+    ITEMS_NUM = 15
+    
+    p_index = request.GET.get('p')
+    if not p_index: 
+        p_index = 1
+    else:
+        p_index = int(p_index)
+    if p_index <= 1: 
+        p_index = 1
+        newer = None
+    else: 
+        newer = p_index - 1
+    older = p_index + 1
+    
+    p_itempos = (p_index - 1) * ITEMS_NUM
+    p_itemnum = p_index * ITEMS_NUM
+    
+    visitQ = Renjianer.objects.order_by('-date')
+    visitnum = visitQ.__len__()
+    if (visitnum / ITEMS_NUM + 1) == p_index:
+        older = None
+        p_itemnum = visitnum
+
+    pageQ = Renjianer.objects.order_by('-date')[p_itempos:p_itemnum]
+
+    userQ = Renjianer.objects.all()
+    userQ.query.group_by = ['user_name']
+    usernum = userQ.__len__()
+    
+    template_parameters = {
+        'newer': newer,
+        'older': older,
+        'visitnum': visitnum,
+        'p_itempos': p_itempos + 1,
+        'p_itemnum': p_itemnum,
+        'usernum': usernum, 
+        'rens': pageQ
+    }
+    return render_to_response('history.html', template_parameters)
